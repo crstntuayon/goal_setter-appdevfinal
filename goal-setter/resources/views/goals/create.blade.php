03baef2af1d4f80b227cf6a657fb280a61704637
crese <crestuayon@gmail.com>
2025-06-15 09:43:07 +0800

    Add See Goals button

--- goal-setter/resources/views/goals/create.blade.php
+++ goal-setter/resources/views/goals/create.blade.php
@@ -7,6 +7,20 @@
     <input type="text" name="title" placeholder="Title" class="border p-2 w-full" required>
     <textarea name="description" placeholder="Description" class="border p-2 w-full"></textarea>
     <input type="number" name="target" placeholder="Target value" class="border p-2 w-full" required>
-    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Save Goal</button>
+    
+
+    <div class="d-flex gap-2">
+    <button type="submit" class="bg-blue-500 text-white px-3 py-2 rounded">Save Goal</button>
+    <a href="{{ route('goals.index') }}" class="bg-yellow-500 text-white px-3 py-2 rounded">See Goals</a>
+</div>
+    <div class="text-red-500">
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
 </form>
 @endsection
