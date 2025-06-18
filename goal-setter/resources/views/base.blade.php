5bcf3a7443a45c8f17f7840225af6fe683c3e130
crese <crestuayon@gmail.com>
2025-06-18 09:39:35 +0800

    Update UI

--- goal-setter/resources/views/base.blade.php
+++ goal-setter/resources/views/base.blade.php
@@ -232,10 +232,24 @@
     </style>
 </head>
 
-<body></body> class="container-fluid">
+<body>
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<style>
+    body {
+        background-color: #f8f9fa;
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        font-family: Arial, sans-serif;
+    }
+
+   
+
+   
+</style>
+
 </body>
 
 </html>
